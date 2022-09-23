e52a9a36627830fb3e3121b1aa63f1cbfed69d1c
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-23 08:46:57 +0200

    initializing method

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout rLayout;
+    private TextView tw;
+    private Button plusbutton;
+    private Button minusbutton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+    }
+
+    private void init(){
+        rLayout = findViewById(R.id.rLayout);
+        tw = findViewById(R.id.tw);
+        plusbutton = findViewById(R.id.plusbutton);
+        minusbutton = findViewById(R.id.minusbutton);
     }
 }
