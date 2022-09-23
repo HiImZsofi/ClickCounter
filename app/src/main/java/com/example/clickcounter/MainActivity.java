1b0599d784fa3ef8500b3a7fbf356aa6cf6fec71
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-23 08:58:54 +0200

    counting method

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.clickcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tw;
     private Button plusbutton;
     private Button minusbutton;
+    public int click;
 
 
 
@@ -23,6 +26,43 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        plusbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click++;
+                if (click > 0){
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(0, 255, 0));
+                }
+                else if(click < 0){
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(255, 0, 0));
+                }
+                else{
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(255, 255, 255));
+                }
+            }
+        });
+
+        minusbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click--;
+                if (click > 0){
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(0, 255, 0));
+                }
+                else if(click < 0){
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(255, 0, 0));
+                }
+                else{
+                    tw.setText(click);
+                    tw.setTextColor(Color.rgb(255, 255, 255));
+                }
+            }
+        });
 
     }
 
@@ -31,5 +71,6 @@ public class MainActivity extends AppCompatActivity {
         tw = findViewById(R.id.tw);
         plusbutton = findViewById(R.id.plusbutton);
         minusbutton = findViewById(R.id.minusbutton);
+        click = 0;
     }
 }
